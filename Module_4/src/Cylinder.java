03ac2b5a1293d800e0f89cfa7b14b7da6b294915
josehernandez <josehernandez@localhost>
2024-03-24 21:39:37 -0500

    test

---
+++ Module_4/src/Cylinder.java
@@ -0,0 +1,29 @@
+public class Cylinder extends Shape {
+    private double radius;
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    @Override
+    public double surfaceArea() {
+        return 2 * Math.PI * radius * (radius + height);
+    }
+
+    @Override
+    public double volume() {
+        return Math.PI * radius * radius * height;
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder{" +
+               "radius=" + radius +
+               ", height=" + height +
+               ", surfaceArea=" + surfaceArea() +
+               ", volume=" + volume() +
+               '}';
+    }
+}
---
+++ Module_4/src/Sphere.java
@@ -0,0 +1,26 @@
+public class Sphere extends Shape {
+    private double radius;
+
+    public Sphere(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double surfaceArea() {
+        return 4 * Math.PI * radius * radius;
+    }
+
+    @Override
+    public double volume() {
+        return (4.0 / 3) * Math.PI * Math.pow(radius, 3);
+    }
+
+    @Override
+    public String toString() {
+        return "Sphere{" +
+               "radius=" + radius +
+               ", surfaceArea=" + surfaceArea() +
+               ", volume=" + volume() +
+               '}';
+    }
+}
